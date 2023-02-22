bd567998893be6fca19c28ffbcff82278f6425d6
estral <105176253+official-estral-esports@users.noreply.github.com>
2023-02-22 17:04:42 -0500

    Delete working directory

--- working/js/ga.js
+++
@@ -1,9 +0,0 @@
-function loadtracking() {
-    window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'UA-104993254-6', {'anonymize_ip': true });
-}
-
-loadtracking();
-
